6fbe39c9b8ff56935a6f125c595e4202e3cd7ac0
wangcch <wangcch.cc@gmail.com>
2021-05-11 17:07:40 +0800

    test: run plugin and verify file

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,14 @@
 import test from "ava";
+import fs from "fs";
+import mock from "mock-fs";
 
 import VersionPlugin from "../src";
+
+test.beforeEach(() => {
+  mock({
+    "/dir/repo": {},
+  });
+});
 
 test("base exist", (t) => {
   const plugin = new VersionPlugin();
@@ -25,4 +33,36 @@ test("options", (t) => {
 
   t.is(plugin.options.output, "version/index.txt");
   t.deepEqual(plugin.options.data, { 1: "demo01", b: "demo02" });
+});
+
+test("apply()", (t) => {
+  const plugin01 = new VersionPlugin({
+    data: { 1: "demo01", b: "demo02" },
+  });
+
+  t.is(plugin01.options.output, "__version.json");
+  t.deepEqual(plugin01.options.data, { 1: "demo01", b: "demo02" });
+
+  t.is(fs.existsSync("/dir/repo01/__version.json"), false);
+
+  plugin01.apply({ options: { output: { path: "/dir/repo01" } } } as any);
+
+  t.is(fs.existsSync("/dir/repo01/__version.json"), true);
+
+  const plugin02 = new VersionPlugin({
+    output: "/version/index.txt",
+    absolute: true,
+    data: { 1: "demo01", b: "demo02" },
+  });
+
+  t.is(plugin02.options.output, "/version/index.txt");
+  t.deepEqual(plugin02.options.data, { 1: "demo01", b: "demo02" });
+
+  t.is(fs.existsSync("/dir/repo02/version/index.txt"), false);
+  t.is(fs.existsSync("/version/index.txt"), false);
+
+  plugin02.apply({ options: { output: { path: "/dir/repo02" } } } as any);
+
+  t.is(fs.existsSync("/dir/repo02/version/index.txt"), false);
+  t.is(fs.existsSync("/version/index.txt"), true);
 });
